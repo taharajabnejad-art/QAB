dde8a2c614f36989f169f7a1708d59d1c9ca97eb
taharajabnejad-art <taharajabnejad@gmail.com>
2026-01-03 22:46:22 +0330

    Update sw-pwa.js

--- sw-pwa.js
+++ sw-pwa.js
@@ -1,14 +1,22 @@
-const CACHE = "pwa-v1";
+const CACHE_NAME = "pwa-v1";
+const FILES_TO_CACHE = [
+  "./test.html",
+  "./test.css",
+  "./pwa.js"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll([
-        "./test.html",
-        "./test.css",
-        "./pwa.js"
-      ])
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting()) // فوراً فعال
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    ).then(() => self.clients.claim())
   );
 });
 
